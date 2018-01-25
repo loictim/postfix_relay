5ad9d6d6e61d7de9a51db7516ad4a9e96ae3a6d3
loictim <loictim@users.noreply.github.com>
2018-01-25 20:52:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env ash
+#!/usr/bin/env ash
 set -e # exit on error
 
 
@@ -7,28 +7,25 @@ if [ -z "$HOSTNAME"  ] ; then
 	echo "HOSTNAME must be defined"
 	exit 1
 else
+  export HOSTNAME
   postconf -e myhostname="$HOSTNAME"
   echo "$HOSTNAME" > /etc/mailname
 fi
 export PROTOCOL=${PROTOCOL:-"ipv4"}
 export INTERFACE=${INTERFACE:-"all"}
 export BANNER=${BANNER:-"$HOSTNAME ESMTP."}
+export MYNETWORKS=${MYNETWORKS:-"127.0.0.1/32"}
 
-if [ -z "$MYNETWORKS"  ] ; then
-	postconf -e "mynetworks=127.0.0.1/32"
-else
-  postconf -e mynetworks="$MYNETWORKS"
-fi
-
-postconf -e inet_protocols=$PROTOCOL
-postconf -e inet_interfaces=$INTERFACE
+postconf -e "mynetworks=$MYNETWORKS"
+postconf -e "inet_protocols=$PROTOCOL"
+postconf -e "inet_interfaces=$INTERFACE"
 postconf -e recipient_delimiter=+
 postconf -e mailbox_size_limit=0
-postconf -e alias_database=hash:/etc/postfix/aliases
-postconf -e alias_maps=hash:/etc/postfix/aliases
+postconf -e "alias_database=hash:/etc/postfix/aliases"
+postconf -e "alias_maps=hash:/etc/postfix/aliases"
 postconf -e smtputf8_enable=no
 postconf -e smtp_use_tls=no
 
 postconf -e "smtpd_banner=$BANNER"
 
-/usr/sbin/postfix  start
+exec /usr/sbin/postfix  start
